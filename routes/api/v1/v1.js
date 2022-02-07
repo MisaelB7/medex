e16d4dddf869494e0e45b57fb39969041421e0f3
Abdiel Banegas <misaelbanegas07@gmail.com>
2022-02-06 21:13:39 -0600

    creacion del apitoken

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pacientesRoutes = require('./pacientes/pacientes');
 const expedientesRoutes = require('./expedientes/expedientes');
+const { verifyApiHeaderToken } = require('./headerVerifyMiddleware');
 //middlewares
-router.use('/pacientes', pacientesRoutes);
-router.use('/expedientes', expedientesRoutes);
+router.use('/pacientes', verifyApiHeaderToken , pacientesRoutes);
+router.use('/expedientes', verifyApiHeaderToken , expedientesRoutes);
 module.exports = router;
+
